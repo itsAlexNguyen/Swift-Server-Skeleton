074fcb458a434b3ee4338283f2e6a42c349a2543
Alex Nguyen <alex.nguyen@td.com>
2017-04-10 10:13:13 -0400

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-server-skeleton",
+    dependencies: [
+      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
+      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
+    ]
+)
